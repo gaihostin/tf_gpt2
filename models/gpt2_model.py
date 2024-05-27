384abfa0ae6207ff67d256ef43b88199ee9a3cb0
gaihostin <liyanan0904@126.com>
2024-05-28 00:24:37 +0800

    add gpt2

---
+++ models/gpt2_model.py
@@ -0,0 +1,22 @@
+import os 
+from tensorflow.python.framework import tensor_shape 
+
+
+
+
+class Gpt2(tf.keras.Model):
+    def __init__(self, num_layers, 
+                 d_model, 
+                 num_heads, 
+                 dff, 
+                 max_seq_len, 
+                 vocab_size, 
+                 optimizer="adam", 
+                 learning_rate=1e-3,
+                 rev_embedding_projection=True, 
+                 grad_clip=False, 
+                 clip_value=1.0):
+        super(Gpt2, self).__init__()
+        pass 
+    
+
